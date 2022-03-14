b4ffcdb71c6d738a90108af34ebca364d1bf4de2
SillyWaffles694 <98199005+SillyWaffles694@users.noreply.github.com>
2022-03-13 23:53:12 -0500

    Add files via upload

---
+++ FTC_TEST.java
@@ -0,0 +1,29 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="WaffleBetter" , group="TeleOp")
+public class FTC_Test {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
